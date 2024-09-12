63679b8ca437957c5b9bcf4960fbd03fc6459047
samimiaari95 <kousa@kousa>
2024-09-12 13:10:53 +0200

    postprocess outputs of pixels in EU

--- postprocess_LSTMoutput.py
+++ postprocess_LSTMoutput.py
@@ -6,83 +6,32 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
-from plot_functions import plot_results, plot_diff, plot_MSE, correlation_map, calc_plot_bias, plot_Europe_avg
+from plot_functions import plot_results, plot_diff, plot_MSE, correlation_map, calc_plot_bias, plot_Europe_avg, pixel_correlation_map, pixels_biasmap, pixel_plot_MSE, chosenpixels_in_EU, chosenpixels_heatmap
 import matplotlib.dates as mdates
 import matplotlib.patches as mpatches
 from LSTM_setup import *
-
-    
-def prepare_inputfeatures(start, end, means_stds):
-    all_inputs = np.array([])
-    for inputvar in FEATURES_FILES:
-        print(inputvar)
-        raw_data = np.load(os.path.join(INPUTPATH, inputvar))
-        raw_data = raw_data[:,:,:]
-        data = raw_data.reshape(raw_data.shape[0], NB_CELLS)
-        data = data[start:end, :]
-        data = np.moveaxis(data, 0, -1) # (cells, timeseries)
-        
-        if f"{inputvar.replace('.npy','')}mean" not in means_stds.keys():
-            means_stds[f"{inputvar.replace('.npy','')}mean"] = np.mean(data)
-            means_stds[f"{inputvar.replace('.npy','')}std"] = np.std(data)
-
-        data = (data - means_stds[f"{inputvar.replace('.npy','')}mean"])/means_stds[f"{inputvar.replace('.npy','')}std"]
-        
-        lookback_arrays = [data[:, i-LOOKBACK:i] for i in range(LOOKBACK, end-start)]
-        lookback_arrays = np.array(lookback_arrays)
-
-        f1 = lookback_arrays.reshape(-1, LOOKBACK)
-        f1 = np.array([f1])
-        all_inputs = np.concatenate((all_inputs,f1), axis=0) if len(all_inputs)>0 else f1
-
-    all_inputs = np.moveaxis(all_inputs, 0, -1)
-    
-    #print(np.unique(np.equal(raw_data[LOOKBACK-1], lookback_arrays[0,:,-1].reshape(X,Y))))
-    #print(np.unique(np.equal(raw_data[LOOKBACK-1], f1[:,-1].reshape(TRAINING_PERIOD-LOOKBACK,X,Y)[0,:,:])))
-    #print(np.unique(np.equal(raw_data[LOOKBACK-1], all_inputs[:,-1, -1].reshape(TRAINING_PERIOD-LOOKBACK,X,Y)[0,:,:])))
-
-    return all_inputs, means_stds
-
-def prepare_targetvar(start, end, means_stds):
-    raw_data = np.load(os.path.join(INPUTPATH, TARGETVAR_FILE))
-    raw_data = raw_data[:,:,:]
-    raw_data = np.nan_to_num(raw_data)
-    raw_data[raw_data < 0.0] = 0
-
-    data = raw_data[start:end, :, :]
-    
-    if f"{TARGETVAR_FILE.replace('.npy','')}mean" not in means_stds.keys():
-        means_stds[f"{TARGETVAR_FILE.replace('.npy','')}mean"] = np.mean(data)
-        means_stds[f"{TARGETVAR_FILE.replace('.npy','')}std"] = np.std(data)
-
-    data = (data - means_stds[f"{TARGETVAR_FILE.replace('.npy','')}mean"])/means_stds[f"{TARGETVAR_FILE.replace('.npy','')}std"]
-    raw_data = (raw_data - means_stds[f"{TARGETVAR_FILE.replace('.npy','')}mean"])/means_stds[f"{TARGETVAR_FILE.replace('.npy','')}std"]
-
-    data = data.flatten()
-
-    #print(np.unique(np.equal(raw_data[start, :, :], data.reshape(end-start, X, Y)[0,:,:])))
-    return data, means_stds
+from utils import singleregion_inputfeatures, singleregion_targetvar, multiregion_inputfeatures, multiregion_targetvar, meanstd_inputfeatures, meanstd_targetvar
 
 
-def load_LSTM(model_path):
+def load_LSTM(source_path, model_path):
     # define mean and std dictionary
     means_stds = {}
     # prepare input data, standardization, lookback and train time series
-    train_inputs, means_stds = prepare_inputfeatures(0, TRAINING_PERIOD, means_stds)
-
+    #train_inputs, means_stds = singleregion_inputfeatures(0, TRAINING_PERIOD, means_stds)
+    #train_inputs, means_stds = multiregion_inputfeatures(0, TRAINING_PERIOD, means_stds, source_path)
+    means_stds = meanstd_inputfeatures(0, TRAINING_PERIOD, means_stds, source_path)
     # prepare input data of target variable and standardize
-    obs_stand_train, means_stds = prepare_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds)
+    #obs_stand_train, means_stds = singleregion_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds)
+    #obs_stand_train, means_stds = multiregion_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds, source_path)
+    means_stds = meanstd_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds, source_path)
 
     # load the model
     lstm_model = torch.load(model_path)
-    #lstm_model = torch.load(os.path.join(os.path.dirname(DIRPATH), SOURCE_REGION, f'{SOURCE_REGION}_wtd_test.pt'))
-    #lstm_model = torch.load(os.path.join(DIRPATH, f'{TARGET_REGION}_wtd_test.pt'))
-    #lstm_model = torch.load(os.path.join(DIRPATH, f'wtd_{SOURCE_REGION}_TL_FT{TARGET_REGION}.pt'))
     criterion = nn.MSELoss()
 
     ################# Testing ######################
-    obs_stand_input, means_stds = prepare_targetvar(TRAINING_PERIOD+LOOKBACK, TRAINING_PERIOD+TEST_PERIOD, means_stds)
-    features_stand_inputs, means_stds = prepare_inputfeatures(TRAINING_PERIOD, TRAINING_PERIOD+TEST_PERIOD, means_stds)
+    obs_stand_input, means_stds = singleregion_targetvar(TRAINING_PERIOD+LOOKBACK, TRAINING_PERIOD+TEST_PERIOD, means_stds)
+    features_stand_inputs, means_stds = singleregion_inputfeatures(TRAINING_PERIOD, TRAINING_PERIOD+TEST_PERIOD, means_stds)
 
     print("preparing dataloader")
     print(f"inputs shape: {features_stand_inputs.shape}")
@@ -120,49 +69,6 @@ def load_LSTM(model_path):
     np.save(os.path.join(OUTPUTPATH, "obs_destand.npy"), obs_destand)
     np.save(os.path.join(OUTPUTPATH, "sim_destand.npy"), sim_destand)
     return obs_destand, sim_destand, obs_stand, sim_stand
-
-
-def finetune_LSTM():
-    # define mean and std dictionary
-    means_stds = {}
-    # prepare input data, standardization, lookback and train time series
-    train_inputs, means_stds = prepare_inputfeatures(0, TRAINING_PERIOD, means_stds)
-
-    # prepare input data of target variable and standardize
-    obs_stand_train, means_stds = prepare_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds)
-
-    # load the model
-    lstm_model = torch.load(os.path.join(os.path.dirname(OUTPUTPATH), SOURCE_REGION, f'{SOURCE_REGION}_wtd.pt'))
-
-    print("creating dataloader")
-    print(f"train features shape: {train_inputs.shape}") # (5*5*timeseries, lookback, features)
-    print(f"train target shape: {obs_stand_train.shape}")
-
-    dataset = TensorDataset(torch.tensor(train_inputs).float(), torch.tensor(obs_stand_train).float())
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-
-    # initialization
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(lstm_model.parameters(), lr=LEARNING_RATE)
-
-    # training
-    print("training")
-    lstm_model.train()
-    epoch_loss = {epoch:[] for epoch in range(NUM_EPOCHS)}
-    for epoch in range(NUM_EPOCHS):
-        for inputs, y in dataloader:
-            optimizer.zero_grad()
-            y_hat = lstm_model(inputs)
-            loss = criterion(y_hat.flatten(), y)
-            loss.backward()
-            optimizer.step()
-            print(f"epoch: {epoch}, loss={loss.item()}")
-            epoch_loss[epoch].append(loss.item())
-
-    # save the trained model
-    torch.save(lstm_model, os.path.join(OUTPUTPATH, f'wtd_{SOURCE_REGION}_TL_FT{TARGET_REGION}.pt'))
-    print("fine-tuned model saved")
-    return
     
 def benchmark_vs_TL():
     print("preparing benchmark model")
@@ -178,9 +84,9 @@ def benchmark_vs_TL():
 
     plot_MSE(cell_mse, TARGET_REGION, f"MSE_{SOURCE_REGION}_TL_FT{TARGET_REGION}_BMvsTL","MSE")
 
-def calc_MSE(obs_destand_test, sim_destand_test):
+def calc_2Dheatmap_MSE(obs, sim):
     criterion = nn.MSELoss()
-    cell_mse = [criterion(torch.tensor(obs_destand_test[:,i]).float(), torch.tensor(sim_destand_test[:,i]).float()).item() for i in range(NB_CELLS)]
+    cell_mse = [criterion(torch.tensor(obs[:,i]).float(), torch.tensor(sim[:,i]).float()).item() for i in range(obs.shape[1])]
     cell_mse = np.array(cell_mse)
     cell_mse = cell_mse.reshape(X,Y)
     return cell_mse
@@ -216,14 +122,8 @@ def load_results():
     lons = np.load(os.path.join(INPUTPATH, "lon2D.npy"))
     lats = np.load(os.path.join(INPUTPATH, "lat2D.npy"))
 
-    tl = False
-    TL_title = f"{SOURCE_REGION}_TL_FT" if tl else ""
-    if tl:
-        obs_destand_test = np.load(os.path.join(OUTPUTPATH, "sim_destand.npy"))
-        sim_destand_test = np.load(os.path.join(OUTPUTPATH, "sim_destand_TL.npy"))    
-    else:
-        obs_destand_test = np.load(os.path.join(OUTPUTPATH, f"obs_destand.npy"))
-        sim_destand_test = np.load(os.path.join(OUTPUTPATH, f"sim_destand.npy"))
+    obs_destand_test = np.load(os.path.join(OUTPUTPATH, f"obs_destand.npy"))
+    sim_destand_test = np.load(os.path.join(OUTPUTPATH, f"sim_destand.npy"))
     
     #convert units to meter
     obs_destand_test = obs_destand_test/1000
@@ -236,34 +136,28 @@ def load_results():
     sim_destand_test[sim_destand_test < 0.01] = 0.0
 
 
-    correlation_map(obs_destand_test, sim_destand_test, f"correlation_{TL_title}{TARGET_REGION}", X, Y, lons, lats)
-    calc_plot_bias(obs_destand_test, sim_destand_test, f"bias_{TL_title}{TARGET_REGION}", X, Y, lons, lats)
-    #plot_blenaltman(obs_destand_test, sim_destand_test, TARGET_REGION, f"{model}_biasBA_{TL_title}{TARGET_REGION}", X, Y)
-    mse_map = calc_MSE(obs_destand_test, sim_destand_test)
-    plot_MSE(mse_map, f"MSE_{TL_title}{TARGET_REGION}", "MSE", lons, lats)
+    correlation_map(obs_destand_test, sim_destand_test, f"correlation_{TARGET_REGION}", X, Y, lons, lats)
+    #pixel_correlation_map(obs_destand_test, sim_destand_test, f"correlation_{TARGET_REGION}", X, Y)
+    #pixels_biasmap(obs_destand_test, sim_destand_test, f"bias_{TARGET_REGION}", X, Y)
+    calc_plot_bias(obs_destand_test, sim_destand_test, f"bias_{TARGET_REGION}", X, Y, lons, lats)
+    #plot_blenaltman(obs_destand_test, sim_destand_test, TARGET_REGION, f"{model}_biasBA_{title}{TARGET_REGION}", X, Y)
+    mse_map = calc_2Dheatmap_MSE(obs_destand_test, sim_destand_test)
+    plot_MSE(mse_map, f"MSE_{TARGET_REGION}", "MSE", lons, lats)
+    #pixel_plot_MSE(mse_map, f"MSE_{TARGET_REGION}", "MSE")
 
     obs_destand_test_avg = calc_datamap(obs_destand_test)
     sim_destand_test_avg = calc_datamap(sim_destand_test)
-    diff_destand_test_avg = calc_diffmap(obs_destand_test_avg, sim_destand_test_avg)
+    #diff_destand_test_avg = calc_diffmap(obs_destand_test_avg, sim_destand_test_avg)
 
-    plot_results(obs_destand_test_avg, f"avgwtd_obs_{TL_title}{TARGET_REGION}","Water table depth (m)", lons, lats)
+    plot_results(obs_destand_test_avg, f"avgwtd_obs_{TARGET_REGION}","Water table depth (m)", lons, lats)
 
-    plot_results(sim_destand_test_avg, f"avgwtd_sim_{TL_title}{TARGET_REGION}", "Water table depth (m)", lons, lats)
+    plot_results(sim_destand_test_avg, f"avgwtd_sim_{TARGET_REGION}", "Water table depth (m)", lons, lats)
 
-    #plot_diff(diff_destand_test_avg, f"avgwtd_diff_{TL_title}{TARGET_REGION}", "Water table depth (m)", lons, lats)
+    #plot_diff(diff_destand_test_avg, f"avgwtd_diff_{title}{TARGET_REGION}", "Water table depth (m)", lons, lats)
 
 def timeseries_plot(pixel):
-    lons = np.load(os.path.join(INPUTPATH, "lon2D.npy"))
-    lats = np.load(os.path.join(INPUTPATH, "lat2D.npy"))
-
-    tl = False
-    TL_title = f"{SOURCE_REGION}_TL_FT" if tl else ""
-    if tl:
-        obs_destand_test = np.load(os.path.join(OUTPUTPATH, "sim_destand.npy"))
-        sim_destand_test = np.load(os.path.join(OUTPUTPATH, "sim_destand_TL.npy"))    
-    else:
-        obs_destand_test = np.load(os.path.join(OUTPUTPATH, "obs_destand.npy"))
-        sim_destand_test = np.load(os.path.join(OUTPUTPATH, "sim_destand.npy"))
+    obs_destand_test = np.load(os.path.join(OUTPUTPATH, "batch_EU_px_training2_wtd_49px_32_prvpdTxTnsmxyindlonlat", "obs_destand.npy"))
+    sim_destand_test = np.load(os.path.join(OUTPUTPATH, "batch_EU_px_training2_wtd_49px_32_prvpdTxTnsmxyindlonlat", "sim_destand.npy"))
 
     #convert units to meter
     obs_destand_test = obs_destand_test/1000
@@ -292,14 +186,14 @@ def timeseries_plot(pixel):
     plt.ylabel('Water table depth (m)')
     plt.legend()
     plt.grid()
-    plt.savefig(os.path.join(OUTPUTPATH, f"timeseries_pointseine.png"))
+    plt.savefig(os.path.join(OUTPUTPATH, f"timeseries_{pixel[0]}_{pixel[1]}.png"))
 
 def compare_timeseries_plots(pixel):
     lons = np.load(os.path.join(INPUTPATH, "lon2D.npy"))
     lats = np.load(os.path.join(INPUTPATH, "lat2D.npy"))
 
-    outputs = {"10yrs_model":[], "topoporo":[], "topoporo128":[], "topoporo256":[], "poro64":[], "topo64":[]}
-    labels = {"10yrs_model":"without static (64)", "topoporo":"topography+porosity (64)", "topoporo128":"topography+porosity (128)", "topoporo256":"topography+porosity (256)", "poro64":"porosity (64)", "topo64":"topography (64)"}
+    outputs = {"DOURO_wtd_5x5_100_64_prvpdsmslopexysoilind":[], "SEINE+DOURO_wtd_5x5_100_64_prvpdsmslopexysoilind":[]}
+    labels = {"DOURO_wtd_5x5_100_64_prvpdsmslopexysoilind":"Training region: Seine", "SEINE+DOURO_wtd_5x5_100_64_prvpdsmslopexysoilind":"Training region: Seine+Douro"}
     
     for output in outputs.keys():
         outputs[output] = np.load(os.path.join(OUTPUTPATH, output, "sim_destand.npy"))
@@ -333,7 +227,7 @@ def compare_timeseries_plots(pixel):
     plt.ylabel('Water table depth (m)')
     plt.legend()
     plt.grid()
-    plt.savefig(os.path.join(OUTPUTPATH, f"timeseries_pointseine.png"))
+    plt.savefig(os.path.join(OUTPUTPATH, f"timeseries_{pixel[0]}_{pixel[1]}.png"))
 
 def bias_timeseries_plot(pixel):
     lons = np.load(os.path.join(INPUTPATH, "lon2D.npy"))
@@ -375,3 +269,113 @@ def bias_timeseries_plot(pixel):
     plt.grid()
     plt.savefig(os.path.join(OUTPUTPATH, f"bias_pointseine.png"))
 
+def calc_2Dheatmap_correlation(obs, sim):
+    correlation_map = np.zeros(NB_CELLS)
+    # Iterate over each grid cell
+    for i in range(obs.shape[1]):
+        # Extract the time series for the current grid cell
+        time_series1 = obs[:, i]
+        time_series2 = sim[:, i]
+        # Calculate the Pearson correlation coefficient
+        if np.std(time_series1) > 0 and np.std(time_series2) > 0:  # Avoid division by zero
+            correlation_matrix = np.corrcoef(time_series1, time_series2)
+            r = correlation_matrix[0, 1]
+        else:
+            r = np.nan  # If there's no variation, set correlation to NaN
+        
+        # Store the correlation coefficient in the map
+        correlation_map[i] = r
+    return correlation_map.reshape(X,Y)
+
+def calc_mean_2D_bias(obs, sim):
+    # calculate bias
+    bias_map = np.mean(sim - obs, axis=0)
+    return bias_map
+
+def calc_2D_correlation(obs, sim):
+    correlation_map = np.zeros((obs.shape[1],obs.shape[2]))
+    # Iterate over each grid cell
+    for i in range(obs.shape[1]):
+        for j in range(obs.shape[2]):
+            # Extract the time series for the current grid cell
+            time_series1 = obs[:, i, j]
+            time_series2 = sim[:, i, j]
+            # Calculate the Pearson correlation coefficient
+            if np.std(time_series1) > 0 and np.std(time_series2) > 0:  # Avoid division by zero
+                correlation_matrix = np.corrcoef(time_series1, time_series2)
+                r = correlation_matrix[0, 1]
+            else:
+                r = np.nan  # If there's no variation, set correlation to NaN
+            
+            # Store the correlation coefficient in the map
+            correlation_map[i, j] = r
+    return correlation_map
+
+def calc_2D_MSE(obs, sim, mapping, choices):
+    criterion = nn.MSELoss()
+    mse1D = [criterion(torch.tensor(obs[:,i]).float(), torch.tensor(sim[:,i]).float()).item() for i in range(obs.shape[1])]
+    mse1D = np.array(mse1D)
+    # reshape into 2D
+    mse_choicesmap = np.zeros(mapping.shape)
+    mse_choicesmap[mse_choicesmap==0] = np.nan
+    include = np.where(mapping==1)
+    for i, choice in enumerate(choices):
+        mse_choicesmap[include[0][choice],include[1][choice]] = mse1D[i]
+    return mse_choicesmap
+
+
+def EUpx_results():
+    obs_destand_test = np.load(os.path.join(OUTPUTPATH, f"obs_destand.npy"))
+    sim_destand_test = np.load(os.path.join(OUTPUTPATH, f"sim_destand.npy"))
+    
+    #convert units to meter
+    obs_destand_test = obs_destand_test/1000
+    sim_destand_test = sim_destand_test/1000
+
+    obs_destand_test = np.nan_to_num(obs_destand_test)
+    sim_destand_test = np.nan_to_num(sim_destand_test)
+
+    obs_destand_test[obs_destand_test < 0.01] = 0.0
+    sim_destand_test[sim_destand_test < 0.01] = 0.0
+
+    choices = np.load(os.path.join(INPUTPATH, "choices.npy"))
+    mapping = np.load(os.path.join(os.path.dirname(INPUTPATH), "mapping_px.npy"))
+    ############## for random from scratch ###################
+    included = np.load(os.path.join(INPUTPATH, "included_excl_waterbodies.npy"))
+    mapping = included
+    ############## remove it if not for random from all dataset excluding water bodies ###################    
+    obs = np.zeros((obs_destand_test.shape[0], mapping.shape[0], mapping.shape[1]))
+    obs[obs==0] = np.nan
+    sim = np.zeros((sim_destand_test.shape[0], mapping.shape[0], mapping.shape[1]))
+    sim[sim==0] = np.nan
+
+    include = np.where(mapping==1)
+    for i, choice in enumerate(choices):
+        obs[:,include[0][choice],include[1][choice]] = obs_destand_test[:,i]
+        sim[:,include[0][choice],include[1][choice]] = sim_destand_test[:,i]
+
+    # calculate and plot mean bias
+    mean_bias_2D_map = calc_mean_2D_bias(obs, sim)
+    mean_bias_2D_heatmap = calc_mean_2D_bias(obs_destand_test, sim_destand_test)
+    chosenpixels_in_EU(mean_bias_2D_map, False, -1, 1, f"Bias")
+    chosenpixels_heatmap(mean_bias_2D_heatmap.reshape(X,Y), False, -1, 1, f"Bias")
+
+    # calculate and plot correlation
+    correlation_2D_map = calc_2D_correlation(obs, sim)
+    correlation_2D_heatmap = calc_2Dheatmap_correlation(obs_destand_test, sim_destand_test)
+    chosenpixels_in_EU(correlation_2D_map, False, -1, 1, f"Correlation")
+    chosenpixels_heatmap(correlation_2D_heatmap, False, -1, 1, f"Correlation")
+
+    # calculate and plot MSE
+    mse_2D_heatmap = calc_2Dheatmap_MSE(obs_destand_test, sim_destand_test)
+    mse_2D_map = calc_2D_MSE(obs_destand_test, sim_destand_test, mapping, choices)
+    chosenpixels_in_EU(mse_2D_map, True, 0.001, 10, f"MSE")
+    chosenpixels_heatmap(mse_2D_heatmap, True, 0.001, 10, f"MSE")
+
+#load_LSTM(
+#    os.path.join(os.path.dirname(INPUTPATH), "maxcorr100_EU"),
+#    os.path.join(os.path.dirname(OUTPUTPATH), "maxcorr100_EU", f"maxcorr100_EU_{MODEL_NAME}.pt")
+#    )
+#load_results()
+#timeseries_plot((2,4))
+EUpx_results()
