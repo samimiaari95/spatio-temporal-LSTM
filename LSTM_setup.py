228c9d61cd597fa49132cabce63544523c9fb720
samimiaari95 <kousa@kousa>
2024-10-15 13:14:00 +0200

    update settings, add scheduler

--- LSTM_setup.py
+++ LSTM_setup.py
@@ -1,40 +1,46 @@
 import torch
 import torch.nn as nn
 
-#plt.rcParams.update({'font.size': 22})
-
-# model name
-MODEL_NAME = "wtd_6160_70_32_prvpdTxTnsmxyindlonlat"
-#MODEL_NAME = "wtd_100_128_prvpdsmxyindlonlat"
+print("check model name")
 
 # number of cells
-X = 70
-Y = 88
+X = 10
+Y = 10
 NB_CELLS = X*Y
 
 # study region
-TARGET_REGION = "EU_px_training"#"EU_px_training"
-SOURCE_REGION = "EU_px_training"
+TARGET_REGION = "rand100EU_yravg1mstd_ohe"
+SOURCE_REGION = "rand100EU_yravg1mstd_ohe"
 
 # directory and inputs
-OUTPUTPATH = f"/p/project1/cslts/miaari1/python_scripts/spatio-temporal-LSTM/outputs/{TARGET_REGION}"
-INPUTPATH = f"/p/project1/cslts/miaari1/python_scripts/spatio-temporal-LSTM/inputs/{SOURCE_REGION}"
-FEATURES_FILES = ["TOT_PREC.npy", "vpd.npy", "TMAX_2M.npy", "TMIN_2M.npy", "soilmoisture.npy", "slopex.npy", "slopey.npy", "soilind.npy", "lon2D_ts.npy", "lat2D_ts.npy"]
+OUTPUTPATH = f"/p/project1/cslts/miaari1/python_scripts/spatio-temporal-LSTM/outputs/20yrs_ts/{TARGET_REGION}"
+INPUTPATH = f"/p/project1/cslts/miaari1/python_scripts/spatio-temporal-LSTM/inputs/20yrs_ts/{SOURCE_REGION}"
+FEATURES_FILES = ["TOT_PREC.npy", "vpd.npy", "soilmoisture.npy", "slopex.npy", "slopey.npy", 
+                  "soilind_1.npy", "soilind_2.npy", "soilind_3.npy", "soilind_4.npy", 
+                  "soilind_5.npy", "soilind_6.npy"]
+#FEATURES_FILES = ["TOT_PREC.npy", "vpd.npy", "TMAX_2M.npy", "TMIN_2M.npy", "soilmoisture.npy", "slopex.npy", "slopey.npy", "soilind.npy", "lon2D_ts.npy", "lat2D_ts.npy"]
 TARGETVAR_FILE = "wtd.npy"
 
 # time period
-TRAINING_PERIOD = 365*7
+TRAINING_PERIOD = 365*15
 LOOKBACK = 365
-TEST_PERIOD = 365*2+LOOKBACK
+TEST_PERIOD = 365*4+LOOKBACK
 
 # lstm setup
-INPUT_SIZE = len(FEATURES_FILES) # precip - Tmax - Tmin - soil moisture
-HIDDEN_SIZE = 32
+INPUT_SIZE = len(FEATURES_FILES)
+HIDDEN_SIZE = 160
 NUM_LAYERS = 1
 OUTPUT_SIZE = 1
-NUM_EPOCHS = 70
+NUM_EPOCHS = 100
 LEARNING_RATE = 0.001
+LR_SCHEDULER = False
+LR_STEP_SIZE = 50
+LR_GAMMA = 0.1
 BATCH_SIZE = TRAINING_PERIOD-LOOKBACK
+
+# model name
+MODEL_NAME = f"{X*Y}_{HIDDEN_SIZE}lr{LR_GAMMA.replace('.','')}x{LR_STEP_SIZE}_prvpdsmxyindohe" if LR_SCHEDULER else f"{X*Y}_{HIDDEN_SIZE}_prvpdsmxyindohe"
+
 
 class AwesomeLSTM(nn.Module):
     def __init__(self, INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, NUM_LAYERS):
