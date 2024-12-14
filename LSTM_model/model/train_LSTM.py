401806c0ed663972c874e178987fa82e655f7188
samimiaari95 <kousa@kousa>
2024-12-14 11:08:46 +0100

    implemented DDP

--- LSTM_model/model/train_LSTM.py
+++ LSTM_model/model/train_LSTM.py
@@ -1,16 +1,38 @@
 import os
 import pickle
+import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 from LSTM_model.model.config import *
 from LSTM_model.utils.utils import utilities
 
-class train_LSTM_model:
+class MyDataset(Dataset):
+    def __init__(self, filename):
+        self.datafile = h5py.File(os.path.join(INPUTPATH, filename),'r')
+        self.device = torch.device('cuda')
+    
+    def __len__(self):
+        outputs = self.datafile["target_data"][:]
+        return len(outputs)
+    
+    def __getitem__(self, idx):
+        # all input features in one file of shape (datapoints, lookback, # of features)
+        inputs = self.datafile["input_data"][idx]
+        outputs = self.datafile["target_data"][idx]
+        return torch.tensor(inputs).float().to(self.device), torch.tensor(outputs).float().to(self.device)
+        
+class train_LSTM_model():
 
     def train(self):
+        def get_local_rank():
+            """Return the local rank of this process."""
+            return int(os.getenv('LOCAL_RANK'))
+
+        torch.distributed.init_process_group(backend='cpu:gloo,cuda:nccl')
+
         utils = utilities()
         # check if GPU available in hardware
         if torch.cuda.is_available():
@@ -19,52 +41,72 @@ class train_LSTM_model:
             print("No GPU available. Training will run on CPU.")
 
         # make sure to have cuda toolkit or PyTorch with GPU support installed before this step
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        local_rank = get_local_rank()
+
+        device = torch.device('cuda', local_rank)
+
+        torch.cuda.set_device(device)
 
         # check output directory
         if not os.path.exists(os.path.join(OUTPUTPATH)):
             utils.make_dir(os.path.join(OUTPUTPATH))
 
         # define mean and std dictionary
-        means_stds = {}
+        #means_stds = {}
         # prepare input data, standardization, lookback and train time series
-        train_inputs, means_stds = utils.singleregion_inputfeatures(0, TRAINING_PERIOD, means_stds)
+        #train_inputs, means_stds = utils.singleregion_inputfeatures(0, TRAINING_PERIOD, means_stds)
 
         # prepare input data of target variable and standardize
-        obs_stand_train, means_stds = utils.singleregion_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds)
+        #obs_stand_train, means_stds = utils.singleregion_targetvar(LOOKBACK, TRAINING_PERIOD, means_stds)
 
         # save training data mean and std
-        with open(os.path.join(OUTPUTPATH, f"meanstd_{TARGET_REGION}_{MODEL_NAME}.pkl"), 'wb') as f:
-            pickle.dump(means_stds, f)
+        #with open(os.path.join(OUTPUTPATH, f"meanstd_{TARGET_REGION}_{MODEL_NAME}.pkl"), 'wb') as f:
+        #    pickle.dump(means_stds, f)
+        #f.close()
+
+        #print("creating dataloader")
+        #print(f"train features shape: {train_inputs.shape}") # (30*30*timeseries, lookback, features)
+        #print(f"train target shape: {obs_stand_train.shape}")
+        #print(device)
+        #dataset = TensorDataset(torch.tensor(train_inputs).float().to(device), torch.tensor(obs_stand_train).float().to(device))
+        dataset = MyDataset("target1d_inputs3d_pointsxlookbackxfeatures.h5")
+
+        with open(os.path.join(OUTPUTPATH, f"meanstd_{SOURCE_REGION}_{MODEL_NAME}.pkl"), 'rb') as f:
+            means_stds = pickle.load(f)
         f.close()
 
-        print("creating dataloader")
-        print(f"train features shape: {train_inputs.shape}") # (30*30*timeseries, lookback, features)
-        print(f"train target shape: {obs_stand_train.shape}")
 
-        dataset = TensorDataset(torch.tensor(train_inputs).float().to(device), torch.tensor(obs_stand_train).float().to(device))
-        dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-
+        train_sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True, seed=0,)
+        
+        dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=train_sampler)
         # initialization
         lstm_model = AwesomeLSTM(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, NUM_LAYERS, DROPOUT)
         criterion = nn.MSELoss()
         optimizer = torch.optim.Adam(lstm_model.parameters(), lr=LEARNING_RATE)
         if LR_SCHEDULER: scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=LR_STEP_SIZE, gamma=LR_GAMMA)
 
-        print("training with parallelized GPUs")
-        lstm_model = nn.DataParallel(lstm_model) # Wrap the model with DataParallel
+        #lstm_model = nn.DataParallel(lstm_model) # Wrap the model with DataParallel
         lstm_model = lstm_model.to(device) # Move the model to the GPU
-
+        rank = torch.distributed.get_rank()
+        world_size = torch.distributed.get_world_size()
+        lstm_model = torch.nn.parallel.DistributedDataParallel(
+            lstm_model,
+            device_ids=[rank],
+        )
         # training
         print("training")
         lstm_model.train()
         epoch_loss = {epoch:[] for epoch in range(NUM_EPOCHS)}
         for epoch in range(NUM_EPOCHS):
+            train_sampler.set_epoch(epoch)
             for inputs, y in dataloader:
                 optimizer.zero_grad()
                 y_hat = lstm_model(inputs)
                 loss = criterion(y_hat.flatten(), y)
                 loss.backward()
+                torch.distributed.all_reduce(loss)             
+                loss /= world_size
                 optimizer.step()
                 print(f"epoch: {epoch}, loss={loss.item()}")
                 epoch_loss[epoch].append(loss.item())
@@ -81,3 +123,4 @@ class train_LSTM_model:
             epoch_plot.ylabel("MSE")
             epoch_plot.savefig(os.path.join(OUTPUTPATH, f'{TARGET_REGION}_{MODEL_NAME}.png'))
             print("epoch loss plotted")
+        torch.distributed.destroy_process_group()
