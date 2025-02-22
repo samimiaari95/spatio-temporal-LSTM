3820b2d59de78717dc3bbcb6ce2e0902871cb41d
samimiaari95 <kousa@kousa>
2025-02-22 16:04:36 +0100

    add uncertainty, keep wtd<0.01m

--- LSTM_model/data/postprocess.py
+++ LSTM_model/data/postprocess.py
@@ -5,8 +5,12 @@ import matplotlib.dates as mdates
 import pandas as pd
 import torch
 import torch.nn as nn
-from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 from LSTM_model.utils.plot_functions import plotting_helper
+from LSTM_model.utils.utils import utilities
 from LSTM_model.model.config import *
 
 class postprocess_calculations:
@@ -161,14 +165,15 @@ class postprocess_calculations:
     
     def EUpx_results(self):
         plot_functions = plotting_helper()
+        util = utilities()
         obs_destand_test = np.load(os.path.join(OUTPUTPATH, f"obs_destand_{MODEL_NAME}.npy"))
         sim_destand_test = np.load(os.path.join(OUTPUTPATH, f"sim_destand_{MODEL_NAME}.npy"))
         
         obs_destand_test = np.nan_to_num(obs_destand_test)
         sim_destand_test = np.nan_to_num(sim_destand_test)
 
-        obs_destand_test[obs_destand_test < 0.01] = 0.0
-        sim_destand_test[sim_destand_test < 0.01] = 0.0
+        #obs_destand_test[obs_destand_test < 0.01] = 0.0
+        #sim_destand_test[sim_destand_test < 0.01] = 0.0
 
         mapping = np.load(os.path.join(INPUTPATH, "choices.npy"))
         
@@ -187,7 +192,7 @@ class postprocess_calculations:
         mean_bias_2D_map = self.calc_mean_2D_bias(obs, sim)
         mean_bias_2D_heatmap = self.calc_mean_2D_bias(obs_destand_test, sim_destand_test)
         plot_functions.chosenpixels_in_EU(mean_bias_2D_map, False, -1, 1, f"Bias")
-        plot_functions.chosenpixels_heatmap(mean_bias_2D_heatmap.reshape(X,Y), False, -1, 1, f"Bias")
+        plot_functions.chosenpixels_heatmap(mean_bias_2D_heatmap.reshape(X,Y), False, -10, 10, f"Bias")
 
         # calculate and plot correlation
         correlation_2D_map = self.calc_2D_correlation(obs, sim)
@@ -208,9 +213,27 @@ class postprocess_calculations:
 
         # calculate and plot NSE
         nse_2D_heatmap, nse_2D_map = self.calc_nse(obs_destand_test, sim_destand_test, mapping)
+        nse_2D_heatmap[nse_2D_heatmap<-1] = np.nan
+        nse_2D_map[nse_2D_map<-1] = np.nan
         plot_functions.chosenpixels_in_EU(nse_2D_map, False, -1, 1, f"NSE")
         plot_functions.chosenpixels_heatmap(nse_2D_heatmap, False, -1, 1, f"NSE")
         #plot_functions.plot_predr2(obs_destand_test[:,412], sim_destand_test[:,412])
+
+        # calculate and plot KGE
+        kge_2D_heatmap = [util.calculate_kge(obs_destand_test[:,i], sim_destand_test[:,i]) for i in range(obs_destand_test.shape[1])]
+        kge_2D_heatmap = np.array(kge_2D_heatmap)
+        kge_2D_heatmap = kge_2D_heatmap.reshape(X,Y)
+        kge_2D_map = np.zeros(mapping.shape)
+        kge_2D_heatmap[kge_2D_heatmap<-1] = np.nan
+        for i in range(obs.shape[1]):
+            for j in range(obs.shape[2]):
+                if np.isnan(obs[:,i,j]).all() or np.isnan(sim[:,i,j]).all():
+                    kge_2D_map[i,j] = np.nan
+                else:
+                    kge = util.calculate_kge(obs[:,i,j], sim[:,i,j])
+                    kge_2D_map[i,j] = kge if kge>=-1 else np.nan
+        plot_functions.chosenpixels_in_EU(kge_2D_map, False, -1, 1, f"KGE")
+        plot_functions.chosenpixels_heatmap(kge_2D_heatmap, False, -1, 1, f"KGE")
 
     def calculate_accuracy_parameters(self):
         dirpath = os.path.join(OUTPUTPATH, f"{TARGET_REGION}_{MODEL_NAME}")
@@ -314,14 +337,14 @@ class postprocess_calculations:
 
     def ens_mean(self):
         obs_destand_test = np.load(os.path.join(OUTPUTPATH, f"obs_destand_{MODEL_NAME}.npy"))
-        obs_destand_test[obs_destand_test < 0.01] = 0.0
+        #obs_destand_test[obs_destand_test < 0.01] = 0.0
 
         sim = np.zeros(obs_destand_test.shape)
         for pixel in range(100):
             ens_mean = np.zeros((obs_destand_test.shape[0], 100))
             for m in range(100):
                 sim_destand_test = np.load(os.path.join(os.path.dirname(OUTPUTPATH), f"100px_member_{m}", f"sim_destand_{MODEL_NAME}.npy"))
-                sim_destand_test[sim_destand_test < 0.01] = 0.0
+                #sim_destand_test[sim_destand_test < 0.01] = 0.0
                 ens_mean[:,m] = sim_destand_test[:,pixel]
             sim[:, pixel] = np.mean(ens_mean, axis=1)
         print(obs_destand_test.shape)
@@ -438,3 +461,282 @@ class postprocess_calculations:
         print(sim.shape)
         np.save(os.path.join(os.path.dirname(OUTPUTPATH), "ensemble_weightedRMSE", f"obs_destand_{MODEL_NAME}.npy"), obs_destand_test)
         np.save(os.path.join(os.path.dirname(OUTPUTPATH), "ensemble_weightedRMSE", f"sim_destand_{MODEL_NAME}.npy"), sim)
+
+    def ensemble_statvsacc(self):
+        util = utilities()
+        obs = np.load(os.path.join(OUTPUTPATH, f"obs_destand_{MODEL_NAME}.npy"))
+        meansim = np.load(os.path.join(OUTPUTPATH, f"sim_destand_{MODEL_NAME}.npy"))
+        nb_members = 100
+        members_sim = [np.load(os.path.join(os.path.dirname(OUTPUTPATH), f"400px_member_{m}", f"sim_destand_{MODEL_NAME}.npy")) for m in range(nb_members)]
+        members_sim = np.array(members_sim)
+        members_sim = np.expand_dims(members_sim, axis=0)
+        members_sim = np.concatenate((members_sim), axis=0) #(members, timeseries, pixels)
+        #members_sim = np.moveaxis(members_sim, 0, -1)   # (timeseries, pixels, members)
+        #members_sim[members_sim < 0.01] = 0.0
+
+        x_axis = []
+        y_axis = []
+        ind = 0
+        for pixel in range(100):
+            ensemble_predictions = members_sim[:,:,pixel]
+            ensemble_predictions = np.moveaxis(ensemble_predictions, 0, -1)   # (timeseries, members)
+            # Calculate the mean prediction for each time step
+            mean_prediction = np.mean(ensemble_predictions, axis=1)
+            
+            ########### Calculate ensemble statistics ###########
+            # Calculate the variance for each time step
+            ensemble_variance = np.var(ensemble_predictions, axis=1)
+            ensemble_variance = np.mean(ensemble_variance)
+
+            # Calculate ensemble statistics (e.g., diversity, spread, etc.)
+            # Spread interquantile range (IQR) between 75th and 25th percentiles
+            ensemble_iqr = np.percentile(ensemble_predictions, 75, axis=1) - np.percentile(ensemble_predictions, 25, axis=1)  # IQR
+            ensemble_iqr_mean = np.mean(ensemble_iqr)
+            # Calculate the std for each time step
+            ensemble_std = np.std(ensemble_predictions, axis=1)
+            ensemble_std = np.mean(ensemble_std)
+
+            # Calculate diversity by Pairwise correlation
+            correlation_matrix = np.corrcoef(ensemble_predictions.T)  # Transpose to get members on rows
+            ensemble_diversity = 1 - np.mean(correlation_matrix[np.triu_indices_from(correlation_matrix, k=1)]) # Compute diversity as 1 - average correlation
+
+            
+            ########### Calculate accuracy metrics ###########
+            # Calculate the correlation between the mean prediction and observation
+            correlationobs = np.corrcoef(mean_prediction, obs[:,pixel])[0, 1]
+
+            # Calculate the RMSE between the mean prediction and observation
+            rmse = np.sqrt(np.mean((obs[:,pixel] - mean_prediction) ** 2))
+
+            # Calculate MSE
+
+            # Calculate kingupta
+            kge = util.calculate_kge(obs[:,pixel], mean_prediction)
+            
+            #### KGE terms analysis ####
+            # Compute mean and standard deviation
+            mu_o, mu_p = np.mean(obs[:,pixel]), np.mean(mean_prediction)
+            sigma_o, sigma_p = np.std(obs[:,pixel]), np.std(mean_prediction)
+
+
+            # Compute bias ratio (β) and variability ratio (γ)
+            beta = mu_p / mu_o
+            gamma = sigma_p / sigma_o
+
+            # Calculate NSE
+            nse = 1 - (np.sum((obs[:,pixel] - mean_prediction) ** 2) / np.sum((obs[:,pixel] - np.mean(obs[:,pixel])) ** 2))
+            
+            # Calculate mean bias
+            bias_mean = np.mean(mean_prediction - obs[:,pixel])
+            
+            x_axis.append(ensemble_variance)
+            y_axis.append(abs(bias_mean))
+            
+
+        xtitle = "Variance"
+        ytitle = "Bias"
+
+
+        plt.figure()
+        plt.scatter(x_axis, y_axis, marker='o', color='k')
+        #plt.title(f'{ytitle} vs {xtitle}')
+        plt.xlabel(f'{xtitle}')
+        plt.ylabel(f'{ytitle}')
+        #plt.xlim(0, 1)
+        #plt.ylim(-1, 1)
+        plt.xscale("log")
+        plt.yscale("log")
+        #plt.plot([min(y_axis), max(y_axis)], [min(y_axis), max(y_axis)], color='red', linestyle='--', label='Ideal Fit (y=x)')
+        plt.grid(True, linestyle='--', alpha=0.7)
+        plt.tight_layout()
+        plt.savefig(os.path.join(OUTPUTPATH, "statistics", f"{ytitle}_{xtitle}.png"))
+    
+    def ensemble_linreg(self):
+        util = utilities()
+        # load data
+        obs = np.load(os.path.join(OUTPUTPATH, f"obs_destand_{MODEL_NAME}.npy"))
+
+        nb_members = 100
+        members_sim = [np.load(os.path.join(os.path.dirname(OUTPUTPATH), f"400px_member_{m}", f"sim_destand_{MODEL_NAME}.npy")) for m in range(nb_members)]
+        members_sim = np.array(members_sim)
+        members_sim = np.expand_dims(members_sim, axis=0)
+        members_sim = np.concatenate((members_sim), axis=0) #(members, timeseries, pixels)
+        #members_sim = np.moveaxis(members_sim, 0, -1)   # (timeseries, pixels, members)
+        #members_sim[members_sim < 0.01] = 0.0 # some predictions have full timeseries less than 0.01 
+
+        # Calculate ensemble mean, variance, and diversity
+        variance = []
+        diversity = []
+        acc_matrix = []
+        
+        for pixel in range(100):
+            ensemble_predictions = members_sim[:,:,pixel]
+            ensemble_predictions = np.moveaxis(ensemble_predictions, 0, -1)   # (timeseries, members)
+            # Calculate the mean prediction for each time step
+            ensemble_mean = np.mean(ensemble_predictions, axis=1)
+            
+            ########### Calculate ensemble statistics ###########
+            # Calculate ensemble spread
+            # Calculate the variance for each time step
+            ensemble_variance = np.var(ensemble_predictions, axis=1)
+            ensemble_variance = np.mean(ensemble_variance)
+            # Spread interquantile range (IQR) between 75th and 25th percentiles
+            ensemble_iqr = np.percentile(ensemble_predictions, 75, axis=1) - np.percentile(ensemble_predictions, 25, axis=1)  # IQR
+            ensemble_iqr_mean = np.mean(ensemble_iqr)
+            # Calculate the std for each time step
+            ensemble_std = np.std(ensemble_predictions, axis=1)
+            ensemble_std = np.mean(ensemble_std)
+
+            # Calculate diversity by Pairwise correlation
+            correlation_matrix = np.corrcoef(ensemble_predictions.T)  # Transpose to get members on rows
+            ensemble_diversity = 1 - np.mean(correlation_matrix[np.triu_indices_from(correlation_matrix, k=1)]) # Compute diversity as 1 - average correlation
+            
+            ########### Calculate accuracy metrics ###########
+            # Calculate the correlation between the mean prediction and observation
+            correlationobs = np.corrcoef(ensemble_mean, obs[:,pixel])[0, 1]
+
+            # Calculate the RMSE between the mean prediction and observation
+            pixel_rmse = np.sqrt(np.mean((obs[:,pixel] - ensemble_mean) ** 2))
+
+            # Calculate kingupta
+            kge = util.calculate_kge(obs[:,pixel], ensemble_mean)
+            #if kge<-1:
+            #    kge = -1
+
+            # Calculate mean bias
+            bias_mean = np.mean(ensemble_mean - obs[:,pixel])
+            if not np.isnan(correlationobs):# and kge >= -1:
+                variance.append(ensemble_variance)
+                diversity.append(ensemble_diversity)
+                acc_matrix.append(correlationobs)
+        
+        # Prepare features and target variable
+        X = np.column_stack((variance, diversity))  # Independent variables
+        y = acc_matrix  # Dependent variable (accuracy metric)
+
+        # Fit a linear regression model
+        # accuracy_matrix = a * variance + b * diversity + c
+        model = LinearRegression()
+        model.fit(X, y)
+
+        # Predictions
+        y_pred = model.predict(X)
+
+        # Model evaluation
+        r2 = r2_score(y, y_pred)
+        print("Coefficients:", model.coef_)
+        print("Intercept:", model.intercept_)
+        print("Mean Squared Error:", mean_squared_error(y, y_pred))
+        print("R² Score:", r2)
+
+        # Plot results
+        acc = "correlation"
+        plt.figure(figsize=(10, 6))
+        plt.scatter(y, y_pred, color='blue', alpha=0.7, label='Predicted vs. Actual')
+        plt.plot([min(y), max(y)], [min(y), max(y)], color='red', linestyle='--', label='Ideal Fit (y=x)')
+        plt.xlabel(f"Actual {acc}")
+        plt.ylabel(f"Predicted {acc}")
+        plt.title(f"Linear Regression: {acc} vs Ensemble variance & diversity")
+        #plt.xscale("log")
+        #plt.yscale("log")
+        plt.legend([f"R²={round(r2, 2)}"])
+        plt.grid(True)
+        plt.savefig(os.path.join(OUTPUTPATH, "statistics", f"linreg_{acc}.png"))
+
+    def ensemble_nonlinreg(self):
+        util = utilities()
+        # load data
+        obs = np.load(os.path.join(OUTPUTPATH, f"obs_destand_{MODEL_NAME}.npy"))
+
+        nb_members = 100
+        members_sim = [np.load(os.path.join(os.path.dirname(OUTPUTPATH), f"400px_member_{m}", f"sim_destand_{MODEL_NAME}.npy")) for m in range(nb_members)]
+        members_sim = np.array(members_sim)
+        members_sim = np.expand_dims(members_sim, axis=0)
+        members_sim = np.concatenate((members_sim), axis=0) #(members, timeseries, pixels)
+        #members_sim = np.moveaxis(members_sim, 0, -1)   # (timeseries, pixels, members)
+        #members_sim[members_sim < 0.01] = 0.0 # some predictions have full timeseries less than 0.01 
+
+        # Calculate ensemble mean, variance, and diversity
+        variance = []
+        diversity = []
+        acc_matrix = []
+        
+        for pixel in range(100):
+            ensemble_predictions = members_sim[:,:,pixel]
+            ensemble_predictions = np.moveaxis(ensemble_predictions, 0, -1)   # (timeseries, members)
+            # Calculate the mean prediction for each time step
+            ensemble_mean = np.mean(ensemble_predictions, axis=1)
+            
+            ########### Calculate ensemble statistics ###########
+            # Calculate ensemble spread
+            # Calculate the variance for each time step
+            ensemble_variance = np.var(ensemble_predictions, axis=1)
+            ensemble_variance = np.mean(ensemble_variance)
+            # Spread interquantile range (IQR) between 75th and 25th percentiles
+            ensemble_iqr = np.percentile(ensemble_predictions, 75, axis=1) - np.percentile(ensemble_predictions, 25, axis=1)  # IQR
+            ensemble_iqr_mean = np.mean(ensemble_iqr)
+            # Calculate the std for each time step
+            ensemble_std = np.std(ensemble_predictions, axis=1)
+            ensemble_std = np.mean(ensemble_std)
+
+            # Calculate diversity by Pairwise correlation
+            correlation_matrix = np.corrcoef(ensemble_predictions.T)  # Transpose to get members on rows
+            ensemble_diversity = 1 - np.mean(correlation_matrix[np.triu_indices_from(correlation_matrix, k=1)]) # Compute diversity as 1 - average correlation
+            
+            ########### Calculate accuracy metrics ###########
+            # Calculate the correlation between the mean prediction and observation
+            correlationobs = np.corrcoef(ensemble_mean, obs[:,pixel])[0, 1]
+
+            # Calculate the RMSE between the mean prediction and observation
+            pixel_rmse = np.sqrt(np.mean((obs[:,pixel] - ensemble_mean) ** 2))
+
+            # Calculate kingupta
+            kge = util.calculate_kge(obs[:,pixel], ensemble_mean)
+            
+            # Calculate mean bias
+            bias_mean = np.mean(ensemble_mean - obs[:,pixel])
+
+            if not np.isnan(correlationobs) and kge >= -1:
+                variance.append(ensemble_variance)
+                diversity.append(ensemble_diversity)
+                acc_matrix.append(kge)
+        
+        # Prepare features and target variable
+        X = np.column_stack((variance, diversity))  # Independent variables
+        y = acc_matrix  # Dependent variable (accuracy metric)
+
+        # Create a polynomial regression model (degree=2 for non-linearity)
+        # accuracy_matrix = 1 + a * variance + b * diversity + c * variance^2 + e * variance * diversity + d * diversity^2 + f
+        poly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+        poly_model.fit(X, y)
+        #print("Polynomial Model Parameters:", poly_model.get_params())
+        #print("Polynomial Model Intercept:", poly_model.named_steps['linearregression'].intercept_)
+        #print("Polynomial Model Coefficients:", poly_model.named_steps['linearregression'].coef_)
+        #print("Polynomial Model Degree:", poly_model.named_steps['polynomialfeatures'].degree)
+        #print("Polynomial Model Score:", poly_model.score(X, y))
+        #print("Polynomial Model R² Score:", r2_score(y, poly_model.predict(X)))
+        #print("Polynomial Model Mean Squared Error:", mean_squared_error(y, poly_model.predict(X)))
+        #print(len(poly_model.named_steps['linearregression'].coef_))
+        
+        # Predictions
+        y_poly_pred = poly_model.predict(X)
+
+        # Model evaluation
+        r2 = r2_score(y, y_poly_pred)
+        print("Polynomial Model Mean Squared Error:", mean_squared_error(y, y_poly_pred))
+        print("Polynomial Model R² Score:", r2)
+
+        # Plot results
+        acc = "KGE"
+        plt.figure(figsize=(10, 6))
+        plt.scatter(y, y_poly_pred, color='green', alpha=0.7, label='Predicted vs. Actual (Polynomial)')
+        plt.plot([min(y), max(y)], [min(y), max(y)], color='red', linestyle='--', label='Ideal Fit (y=x)')
+        plt.xlabel(f"Actual {acc}")
+        plt.ylabel(f"Predicted {acc}")
+        plt.title(f"Polynomial Regression: {acc} vs Ensemble Statistics")
+        plt.legend([f"R²={round(r2, 2)}"])
+        #plt.xscale("log")
+        #plt.yscale("log")
+        plt.grid(True)
+        #plt.savefig(os.path.join(OUTPUTPATH, "statistics", f"nonlinreg_{acc}.png"))
+
