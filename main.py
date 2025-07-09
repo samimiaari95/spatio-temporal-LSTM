1d5a839b11658b9ef2248bc329c7b0e196ad0be2
samimiaari95 <kousa@kousa>
2025-07-09 17:35:04 +0200

    ensemble validation and consistent font size

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import argparse
+import matplotlib.pyplot as plt
 from LSTM_model.model.config import *
 from LSTM_model.model.train_LSTM import train_LSTM_model
-from LSTM_model.model.validate_LSTM import validate_LSTM_model
+from LSTM_model.model.validate_LSTM import validate_LSTM_model, transfer_ensemble_LSTM
 from LSTM_model.data.postprocess import postprocess_calculations
 from LSTM_model.data.preprocess import preprocessing_data
 from LSTM_model.data.rawdataset import preprocess_rawdata
 from LSTM_model.utils.plot_functions import plotting_helper
 from LSTM_model.utils.utils import utilities
-from LSTM_model.utils.volumetric_soilmoisture import calculate_soilmoisture
+#from LSTM_model.utils.volumetric_soilmoisture import calculate_soilmoisture
 
 
 parser = argparse.ArgumentParser()
@@ -23,6 +24,7 @@ parser.add_argument('-e', '--eval', action='store_true', required=False, help='E
 parser.add_argument('-c', '--calc', action='store_true', required=False, help='Results calculation')
 
 args = parser.parse_args()
+plt.rcParams.update({'font.size': 20})
 
 if args.raw:
     rawdataset = preprocess_rawdata()
@@ -37,9 +39,9 @@ if args.train:
     train_LSTM.train()
 
 if args.eval:
-    validate_LSTM = validate_LSTM_model()
-    validate_LSTM.eval_LSTM()
+    validate_LSTM = transfer_ensemble_LSTM()
+    validate_LSTM.transfer_ensemble()
 
 if args.calc:
     postprocess = postprocess_calculations()
-    postprocess.EUpx_results()
+    # call function here
