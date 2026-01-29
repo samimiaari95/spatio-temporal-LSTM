d74e2ff063b7f1788ff5d7c1e84abfa3fc206a71
samimiaari95 <kousa@kousa>
2026-01-29 12:48:40 +0100

    run only needed commands

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from LSTM_model.model.validate_LSTM import transfer_ensemble_LSTM
 from LSTM_model.data.postprocess import postprocess_calculations
 from LSTM_model.data.preprocess import preprocessing_data
 from LSTM_model.data.rawdataset import preprocess_rawdata
+from LSTM_model.data.obs_processing import obs_processing
 from LSTM_model.utils.plot_functions import plotting_helper
 from LSTM_model.utils.utils import utilities
+import LSTM_model.data.download_ERA5 as download_ERA5
 #from LSTM_model.utils.volumetric_soilmoisture import calculate_soilmoisture
 
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--batchind', type=str, required=True, help='yyyymmdd00') # month of simulation
+parser.add_argument('--batchind', type=str, required=True, help='yyyymmdd00') # month of simulation
+
+parser.add_argument('-d', '--dera', action='store_true', required=False, help='ERA5 data download')
 
 parser.add_argument('-r', '--raw', action='store_true', required=False, help='Rawdata preprocessing')
 
@@ -24,16 +28,28 @@ parser.add_argument('-e', '--eval', action='store_true', required=False, help='E
 
 parser.add_argument('-c', '--calc', action='store_true', required=False, help='Results calculation')
 
+parser.add_argument('-o', '--obs', action='store_true', required=False, help='Observations processing')
+
 args = parser.parse_args()
 plt.rcParams.update({'font.size': 20})
 
+if args.dera:
+    # download_ERA5.request_ERA5_t2m_d2m_swvl3()
+    # download_ERA5.request_ERA5_tp()
+    # download_ERA5.extract_allzipfiles()
+    pass
+
 if args.raw:
     rawdataset = preprocess_rawdata()
+    rawdataset.extract_vars()
     # call function here
 
 if args.prep:
     preprocessing = preprocessing_data()
     # call function here
+    # preprocessing.conc_vars()
+    # preprocessing.ensemble_cropvars()
+    preprocessing.checkoutputs()
 
 if args.train:
     train_LSTM = train_LSTM_model()
@@ -41,9 +57,50 @@ if args.train:
 
 if args.eval:
     validate_LSTM = transfer_ensemble_LSTM()
-    # batchind = args.batchind
-    # validate_LSTM.transfer_ensemble(batchind=batchind)
+    batchind = args.batchind
+    # input_vars = {x: np.load(os.path.join(INPUTPATH, "validation_ERA5", f"target_pixels_{batchind}", x)) for x in FEATURES_FILES}
+    # targetvar = np.load(os.path.join(INPUTPATH, "validation_ERA5", f"target_pixels_{batchind}", TARGETVAR_FILE))
+    # for member in range(100):
+        # validate_LSTM.transfer_ensemble(batchind=batchind, member=member, input_vars=input_vars, targetvar=targetvar)
+    try:
+        validate_LSTM.transfer_ensemble(batchind=batchind)
+    except:
+        # write the batchind and member that failed to a log file
+        with open(os.path.join(OUTPUTPATH, "validation_ERA5", "ensemble_400px", f"failed_batches_{member}.log"), 'a') as f:
+            f.write(f"{batchind}\n")
+        f.close()
 
 if args.calc:
     postprocess = postprocess_calculations()
-    postprocess.ensemble_statvsacc()
+    # postprocess.ens_mean()
+    # postprocess.concat_EU_transfer_testtrain_outputs()
+    # postprocess.cdf_EU()
+    # postprocess.metrics_2D_EU()
+
+    # postprocess.preprocess_crps_trainingEU()
+    # postprocess.preprocess_crps_transferEU()
+    # postprocess.crps_seasonal_trainEU()
+    # postprocess.crps_seasonal_transferEU()
+    # postprocess.boxplot_seasonal_crps()
+    # postprocess.ensemble_statvsacc()
+    # postprocess.ensemble_statvsacc_fitting()
+    # postprocess.crps_transferEU()
+    # postprocess.ensemble_crpsvsstats()
+    # postprocess.ensemble_crpsvsstats_fitting()
+    # postprocess.estimate_acc_from_stats()
+    # postprocess.kge_investigation()
+    # postprocess.kgecomponents_vs_EV()
+    # postprocess.metrics_vs_topo()
+    
+    postprocess.eval_era5wtd_obswtd_tsmpwtd()
+    postprocess.plot2Dmaps()
+    postprocess.fitted_ev_rmse()
+    postprocess.fitted_iqr_mab()
+    postprocess.plot_cdfs()
+    # postprocess.location_of_localobs()
+
+if args.obs:
+    obs_proc = obs_processing()
+    # call function here
+    # obs_proc.read_csv_monthlyobs_from_yueling()
+    # obs_proc.maptoTSMP_averageduplicates()
