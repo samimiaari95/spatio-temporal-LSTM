d0b70e7a6c5d8d5cfa1afe687d7d0636b4fb5385
samimiaari95 <kousa@kousa>
2026-07-07 15:44:45 +0200

    read inputs from hdf5 with dataset class

--- LSTM_model/model/validate_LSTM.py
+++ LSTM_model/model/validate_LSTM.py
@@ -1,12 +1,26 @@
 import numpy as np
 import pickle
-from torch.utils.data import DataLoader, TensorDataset
+import h5py
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 from LSTM_model.model.config import *
 from LSTM_model.utils.utils import utilities
 
+class MyDataset(Dataset):
+    def __init__(self, filename):
+        self.datafile = h5py.File(os.path.join(INPUTPATH, filename),'r')
+        self.device = torch.device('cuda')
+    
+    def __len__(self):
+        return self.datafile["target_data"].shape[0]
+
+    def __getitem__(self, idx):
+        # all input features in one file of shape (datapoints, lookback, # of features)
+        inputs = self.datafile["input_data"][idx]
+        outputs = self.datafile["target_data"][idx]
+        return torch.from_numpy(inputs).float().to(self.device), torch.from_numpy(outputs).float().to(self.device)
 
 class transfer_ensemble_LSTM:
-    def transfer_ensemble(self, batchind):
+    def transfer_ensemble(self):
         def get_local_rank():
             """Return the local rank of this process."""
             return int(os.getenv('LOCAL_RANK'))
@@ -14,9 +28,8 @@ class transfer_ensemble_LSTM:
         torch.distributed.init_process_group(backend='cpu:gloo,cuda:nccl')
 
         logger = logging.getLogger(__name__)
-        logger.warning(f"Transferring for member {member} and batch index {batchind}")
+        logger.warning(f"Transferring for member {member}")
         print(f"starting eval")
-        utils = utilities()
 
         if torch.cuda.is_available():
             print(f"GPU: {torch.cuda.get_device_name(0)} is available.")
@@ -52,15 +65,19 @@ class transfer_ensemble_LSTM:
         )
 
         ################# Testing ######################
-        obs_stand_input, means_stds = utils.transferpx_targetvar(LOOKBACK, TEST_PERIOD, means_stds, batchind)
-        features_stand_inputs, means_stds = utils.transferpx_inputfeatures(0, TEST_PERIOD, means_stds, batchind)
+        # obs_stand_input, means_stds = utils.transferpx_targetvar(0, TEST_PERIOD-LOOKBACK, means_stds, batchind)
+        # usually starting from LOOKBACK to TEST_PERIOD, but here we start from 0 to TEST_PERIOD since we don't provide wtd of TSMP for 1999, 
+        # we don't need it because we will later compare to observations not TSMP
+        # features_stand_inputs, means_stds = utils.transferpx_inputfeatures(0, TEST_PERIOD, means_stds, batchind)
 
         print("preparing dataloader")
-        print(f"inputs shape: {features_stand_inputs.shape}")
-        print(f"obs shape: {obs_stand_input.shape}")
-        test_dataset = TensorDataset(torch.tensor(features_stand_inputs).float().to(device), torch.tensor(obs_stand_input).float().to(device))
+        # print(f"inputs shape: {features_stand_inputs.shape}")
+        # print(f"obs shape: {obs_stand_input.shape}")
+        datasetpath = os.path.join("/p/scratch/cesmtst/miaari1/inputhdf5_files", f"standardized3dinput1dtarget_{member}.h5")
+        test_dataset = MyDataset(datasetpath)
+        # test_dataset = TensorDataset(torch.tensor(features_stand_inputs).float().to(device), torch.tensor(obs_stand_input).float().to(device))
         #test_sampler = torch.utils.data.distributed.DistributedSampler(test_dataset, shuffle=False, seed=0,)
-        test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)#sampler=test_sampler) # no random shuffling for the test
+        test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True)#sampler=test_sampler) # no random shuffling for the test
 
         ## Evaluation
         print("evaluation")
@@ -85,12 +102,12 @@ class transfer_ensemble_LSTM:
 
         # standardization
         # compare it with the original values to confirm the standardization process
-        obs_destand = obs_stand*means_stds[f"{TARGETVAR_FILE.replace('.npy','')}std"] + means_stds[f"{TARGETVAR_FILE.replace('.npy','')}mean"]
-
-        sim_destand = sim_stand*means_stds[f"{TARGETVAR_FILE.replace('.npy','')}std"] + means_stds[f"{TARGETVAR_FILE.replace('.npy','')}mean"]
+        # TODO battabombabav probably the means_std for target are not found in previous and so calculated based on this period
+        obs_destand = obs_stand*means_stds[f"wtdstd"] + means_stds[f"wtdmean"]
+        sim_destand = sim_stand*means_stds[f"wtdstd"] + means_stds[f"wtdmean"]
         print(f"obs destand shape: {obs_destand.shape}")
         print(f"sim destand shape: {sim_destand.shape}")
-        np.save(os.path.join(OUTPUTPATH, "validation_ERA5", "ensemble_400px", f"400px_member_{member}", f"obs_{batchind}.npy"), obs_destand)
-        np.save(os.path.join(OUTPUTPATH, "validation_ERA5", "ensemble_400px", f"400px_member_{member}", f"sim_{batchind}.npy"), sim_destand)
+        np.save(os.path.join(OUTPUTPATH, "validation_ERA5", "ensemble_400px", f"400px_member_{member}", f"obs_localobspixels.npy"), obs_destand)
+        np.save(os.path.join(OUTPUTPATH, "validation_ERA5", "ensemble_400px", f"400px_member_{member}", f"sim_ERA5onlocalobspixels.npy"), sim_destand)
         torch.distributed.destroy_process_group()
         return print("saved destandardized arrays")
